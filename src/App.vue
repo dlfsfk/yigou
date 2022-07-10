2c6ab874a1669d169cf2e7ea9b39055438e1db25
2019112304_董争平 <1490770930@qq.com>
2022-07-10 17:13:45 +0800

    首页

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,59 @@
       <user-name />
     </div>
     <router-view />
+	<!-- <div style="margin-bottom: 150px;">
+	  <a-input-group compact>
+	    <a-select default-value="湖北">
+	      <a-select-option value="Hubei">
+	        湖北
+	      </a-select-option>
+	      <a-select-option value="Hunan">
+	        湖南
+	      </a-select-option>
+			<a-select-option value="Shanghai">
+			  上海
+			</a-select-option>
+			<a-select-option value="Beijing">
+			  北京
+			</a-select-option>
+	    </a-select>
+		<a-input-search style="width: 50%" placeholder="input search text" enter-button @search="onSearch" />
+	  </a-input-group>
+	</div>
+	<div style="opacity: 0.8; width:80%;margin: auto;">
+	  <a-row :gutter="150">
+	    <a-col :span="6">
+	      <a-card title="Card title" :bordered="false" :hoverable="true">
+	        <p>card content</p>
+	      </a-card>
+	    </a-col>
+		<a-col :span="6">
+		  <a-card title="Card title" :bordered="false" :hoverable="true">
+		    <p>card content</p>
+		  </a-card>
+		</a-col>
+	    <a-col :span="6">
+	      <a-card title="Card title" :bordered="false" :hoverable="true">
+	        <p>card content</p>
+	      </a-card>
+	    </a-col>
+	    <a-col :span="6">
+	      <a-card title="Card title" :bordered="false" :hoverable="true">
+			<a-statistic
+			            :value="11.28"
+			            :precision="2"
+			            suffix="%"
+			            :value-style="{ color: '#3f8600' }"
+			          >
+			            <template #prefix>
+			              <a-icon type="arrow-up" />
+			            </template>
+		    </a-statistic>
+	      </a-card>
+	    </a-col>
+	  </a-row>
+	</div> -->
+	
   </div>
 </template>
 
@@ -19,6 +72,11 @@ export default {
   components: {
     UserName,
   },
+  // methods: {
+  //   onSearch(value) {
+  //     console.log(value);
+  //   },
+  // },
 };
 </script>
 
@@ -29,6 +87,7 @@ body {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  background-image: url(assets/banner.jpg);
 }
 #nav {
   padding: 30px;
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,59 @@
 <template>
 <div>
   <h1>Home Page</h1>
-  <div class="banner">
-      <a-input-search placeholder="input search text" enter-button @search="onSearch" />
+  <div style="margin-bottom: 150px;" class="banner">
+    <a-input-group compact>
+      <a-select default-value="湖北">
+        <a-select-option value="Hubei">
+          湖北
+        </a-select-option>
+        <a-select-option value="Hunan">
+          湖南
+        </a-select-option>
+  		<a-select-option value="Shanghai">
+  		  上海
+  		</a-select-option>
+  		<a-select-option value="Beijing">
+  		  北京
+  		</a-select-option>
+      </a-select>
+  	<a-input-search style="width: 50%" placeholder="input search text" enter-button @search="onSearch" />
+    </a-input-group>
   </div>
-
+  <div style="opacity: 0.8; width:80%;margin: auto;">
+    <a-row :gutter="150">
+      <a-col :span="6">
+        <a-card title="Card title" :bordered="false" :hoverable="true">
+          <p>card content</p>
+        </a-card>
+      </a-col>
+  	<a-col :span="6">
+  	  <a-card title="Card title" :bordered="false" :hoverable="true">
+  	    <p>card content</p>
+  	  </a-card>
+  	</a-col>
+      <a-col :span="6">
+        <a-card title="Card title" :bordered="false" :hoverable="true">
+          <p>card content</p>
+        </a-card>
+      </a-col>
+      <a-col :span="6">
+        <a-card title="Card title" :bordered="false" :hoverable="true">
+  		<a-statistic
+  		            :value="11.28"
+  		            :precision="2"
+  		            suffix="%"
+  		            :value-style="{ color: '#3f8600' }"
+  		          >
+  		            <template #prefix>
+  		              <a-icon type="arrow-up" />
+  		            </template>
+  	    </a-statistic>
+        </a-card>
+      </a-col>
+    </a-row>
+  </div>
 </div>
-  
 </template>
 
 <script>
